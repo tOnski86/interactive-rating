df76c47e2d2a22a02ea176f03abe39fb83210d6a
Antonio Saqueton <code.antoniohicetasaqueton@gmail.com>
2024-11-12 08:26:49 +0800

    Add default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,24 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    colors: {
+      orange: 'hsl(25, 97%, 53%)',
+      white: 'hsl(0, 0%, 100%)',
+      grey: 'hsl(217, 12%, 63%)',
+      blue: 'hsl(213, 19%, 18%)',
+      black: 'hsl(216, 12%, 8%)',
+    },
+    fontFamily: {
+      overpass: ['Overpass', 'sans-serif'],
+    },
   },
   plugins: [],
 };
+
+/*
+- Mobile: 375px
+- Desktop: 1440px
+
+- Family: [Overpass](https://fonts.google.com/specimen/Overpass)
+- Weights: 400, 700
+*/
